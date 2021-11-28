0d7a7cc0f76a2f7dbce3f0cd7c86cc5555d71c41
marko410 <marko.penzes03@gmail.com>
2021-11-28 15:52:33 +0100

    Pridanie_uloh

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -205,7 +205,7 @@
     <button class="glow-on-hover" class="next" onclick="next(2)">NEXT</button>
 
 </div>
-
+<!--<otazka3-->
 <div class="container">
     <div class="textik">Koľko litrov krvi prečerpá za deň ľudské srdce?</div><hr>
     <img src="obrázok3.jpg" width="450" height="400">
@@ -225,12 +225,439 @@
         <label for="Odpoved4">7500</label><br>
     </div>
     </div>
+    <button class="glow-on-hover" class="next" onclick="next(3)">NEXT</button>
+            onclick="result()">Vyhodnotiť</button>
+
+</div>
+<!--<otazka4-->
+
+<div class="container">
+    <div class="textik">Ktorý z týchto orgánov ľudského tela nie je párový??</div><hr>
+    <img src="obrázok4.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved4" value="Odpoved 1"  >
+            <label for="Odpoved1">Močovod</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved4" value="Odpoved 2">
+            <label for="Odpoved2">Oblička</label><br>
+
+            <input class="styl" type="radio" id="correct4"  name="odpoved4" value="Odpoved 3">
+            <label for="Odpoved3">Týmus(detská žľaza)</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved4" value="Odpoved 4">
+            <label for="Odpoved4">Pľúca</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(4)">NEXT</button>
+
+</div>
+
+
+<!--<otazka5-->
+
+<div class="container">
+    <div class="textik">Ktorá krvná skupina má vo svete najvyššie zastúpenie medzi obyvateľstvom??</div><hr>
+    <img src="obrázok5.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved5" value="Odpoved 1"  >
+            <label for="Odpoved1">A</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved5" value="Odpoved 2">
+            <label for="Odpoved2">B</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved5" value="Odpoved 3">
+            <label for="Odpoved3">AB</label><br>
+
+            <input class="styl" type="radio" id="correct5"  name="odpoved5" value="Odpoved 4">
+            <label for="Odpoved4">0</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(5)">NEXT</button>
+
+</div>
+
+<!--<otazka6-->
+
+<div class="container">
+    <div class="textik">Koľko rôznych vôní rozozná náš čuch?</div><hr>
+    <img src="obrázok6.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved6" value="Odpoved 1"  >
+            <label for="Odpoved1">20 000</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved6" value="Odpoved 2">
+            <label for="Odpoved2">30 000</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved6" value="Odpoved 3">
+            <label for="Odpoved3">40 000</label><br>
+
+            <input class="styl" type="radio" id="correct6"  name="odpoved6" value="Odpoved 4">
+            <label for="Odpoved4">50 000</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(6)">NEXT</button>
+
+</div>
+
+<!--<otazka7-->
+
+<div class="container">
+    <div class="textik">Ľudský tep sa spomalí v okamihu, keď sa tvár dotkne vody. Ako sa nazýva tento jav?</div><hr>
+    <img src="obrázok7.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved7" value="Odpoved 1"  >
+            <label for="Odpoved1">Reflex topiacich sa</label><br>
+
+            <input class="styl" type="radio" id="correct7"  name="odpoved7" value="Odpoved 2">
+            <label for="Odpoved2">Potápačský reflex</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved7" value="Odpoved 3">
+            <label for="Odpoved3">Obranný vodný reflex</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved7" value="Odpoved 4">
+            <label for="Odpoved4">Dýchací reflex</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(7)">NEXT</button>
+
+</div>
+
+<!--<otazka8-->
+
+<div class="container">
+    <div class="textik">Väzivový útvar, ktorý sa nachádza na lebke malých detí v prvých dvoch rokoch života, sa nazýva:</div><hr>
+    <img src="obrázok8.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="correct8"  name="odpoved8" value="Odpoved 1"  >
+            <label for="Odpoved1">Fontanela</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved8" value="Odpoved 2">
+            <label for="Odpoved2">Fixinela</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved8" value="Odpoved 3">
+            <label for="Odpoved3">Fabio Quagliarella</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved8" value="Odpoved 4">
+            <label for="Odpoved4">Cranionella</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(8)">NEXT</button>
+
+</div>
+<!--<otazka9-->
+
+<div class="container">
+    <div class="textik">Ktoré zuby nenájdete v mliečnom chrupe?</div><hr>
+    <img src="obrázok9.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved9" value="Odpoved 1"  >
+            <label for="Odpoved1">Rezáky</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved9" value="Odpoved 2">
+            <label for="Odpoved2">Očné zuby</label><br>
+
+            <input class="styl" type="radio" id="correct9"  name="odpoved9" value="Odpoved 3">
+            <label for="Odpoved3">Črenové zuby</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved9" value="Odpoved 4">
+            <label for="Odpoved4"> Stoličky</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(9)">NEXT</button>
+
+</div>
+
+<!--<otazka10-->
+
+<div class="container">
+    <div class="textik">Počet stavcov chrbtice človeka je rovnaký ako u žirafy. Koľko ich máme?</div><hr>
+    <img src="obrázok10.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved10" value="Odpoved 1"  >
+            <label for="Odpoved1">25</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved10" value="Odpoved 2">
+            <label for="Odpoved2">30</label><br>
+
+            <input class="styl" type="radio" id="correct10"  name="odpoved10" value="Odpoved 3">
+            <label for="Odpoved3">33</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved10" value="Odpoved 4">
+            <label for="Odpoved4">38</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(10)">NEXT</button>
+
+</div>
+
+<!--<otazka11-->
+
+<div class="container">
+    <div class="textik">Kde nájdeme najmenšie kosti a svaly v ľudskom tele?</div><hr>
+    <img src="obrázok11.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved11" value="Odpoved 1"  >
+            <label for="Odpoved1">V srdci</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved11" value="Odpoved 2">
+            <label for="Odpoved2">V panve</label><br>
+
+            <input class="styl" type="radio" id="correct11"  name="odpoved11" value="Odpoved 3">
+            <label for="Odpoved3">V uchu</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved11" value="Odpoved 4">
+            <label for="Odpoved4">V kolene</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(11)">NEXT</button>
+
+</div>
+
+<!--<otazka12-->
+
+<div class="container">
+    <div class="textik">Akú kosť vidíte na obrázku?</div><hr>
+    <img src="obrázok12.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved12" value="Odpoved 1"  >
+            <label for="Odpoved1">Miechu</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved12" value="Odpoved 2">
+            <label for="Odpoved2">Rebrá</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved12" value="Odpoved 3">
+            <label for="Odpoved3">Kľúčnu kosť</label><br>
+
+            <input class="styl" type="radio" id="correct12"  name="odpoved12" value="Odpoved 4">
+            <label for="Odpoved4">Hrudnú kosť</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(12)">NEXT</button>
+
+</div>
+
+<!--<otazka13-->
+
+<div class="container">
+    <div class="textik">Na obrázku vidíme prístroj, ktorý sa nazýva:</div><hr>
+    <img src="obrázok13.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved13" value="Odpoved 1"  >
+            <label for="Odpoved1">EKG</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved13" value="Odpoved 2">
+            <label for="Odpoved2"> Inhalátor</label><br>
+
+            <input class="styl" type="radio" id="correct13"  name="odpoved13" value="Odpoved 3">
+            <label for="Odpoved3">Dialýza</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved13" value="Odpoved 4">
+            <label for="Odpoved4">Lymfodrenážny prístroj</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(13)">NEXT</button>
+
+</div>
+
+<!--<otazka14-->
+
+<div class="container">
+    <div class="textik">Ako sa nazýva kosť na obrázku?</div><hr>
+    <img src="obrázok14.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved14" value="Odpoved 1"  >
+            <label for="Odpoved1">Lakťová kosť</label><br>
+
+            <input class="styl" type="radio" id="correct14"  name="odpoved14" value="Odpoved 2">
+            <label for="Odpoved2">Vretenná kosť</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved14" value="Odpoved 3">
+            <label for="Odpoved3">Ramenná kosť</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved14" value="Odpoved 4">
+            <label for="Odpoved4">Hubovitá kosť</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(14)">NEXT</button>
+
+</div>
+
+<!--<otazka15-->
+
+<div class="container">
+    <div class="textik">Ako sa nazýva kosť na obrázku?</div><hr>
+    <img src="obrázok15.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved15" value="Odpoved 1"  >
+            <label for="Odpoved1">Holenná kosť</label><br>
+
+            <input class="styl" type="radio" id="correct15"  name="odpoved15" value="Odpoved 2">
+            <label for="Odpoved2">Lýtková kosť</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved15" value="Odpoved 3">
+            <label for="Odpoved3">Kompaktná kosť</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved15" value="Odpoved 4">
+            <label for="Odpoved4">Okostica</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(15)">NEXT</button>
+
+</div>
+
+<!--<otazka16-->
+
+<div class="container">
+    <div class="textik">Kto nie je z uvedených oprávnený predpisovať lieky?</div><hr>
+    <img src="obrázok16.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved16" value="Odpoved 1"  >
+            <label for="Odpoved1">Zubár</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved16" value="Odpoved 2">
+            <label for="Odpoved2">Veterinár</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved16" value="Odpoved 3">
+            <label for="Odpoved3">Sekundárny lekár</label><br>
+
+            <input class="styl" type="radio" id="correct16"  name="odpoved16" value="Odpoved 4">
+            <label for="Odpoved4">Terapeut</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(16)">NEXT</button>
+
+</div>
+
+
+<!--<otazka17-->
+
+<div class="container">
+    <div class="textik">Koľko svalov sa musí zapojiť pri úsmeve?</div><hr>
+    <img src="obrázok17.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved17" value="Odpoved 1"  >
+            <label for="Odpoved1">6</label><br>
+
+            <input class="styl" type="radio" id="correct17"  name="odpoved17" value="Odpoved 2">
+            <label for="Odpoved2">17</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved17" value="Odpoved 3">
+            <label for="Odpoved3">25</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved17" value="Odpoved 4">
+            <label for="Odpoved4">33</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover"  onclick="result()">Vyhodnotiť</button>
+
+</div>
+
+<!--<otazka18-->
+
+<div class="container">
+    <div class="textik">Ako nazývame ochorenie, ktoré sa prejavuje niekoľkosekundovými zástavami dýchania počas noci?</div><hr>
+    <img src="obrázok18.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved18" value="Odpoved 1"  >
+            <label for="Odpoved1">Narkolepsia</label><br>
+
+            <input class="styl" type="radio" id="correct18"  name="odpoved18" value="Odpoved 2">
+            <label for="Odpoved2">Spánkové apnoe</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved18" value="Odpoved 3">
+            <label for="Odpoved3">Spánková paralýza</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4"  name="odpoved18" value="Odpoved 4">
+            <label for="Odpoved4">Nočná mora</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(17)">NEXT</button>
+
+</div>
+
+<!--<otazka19-->
+
+<div class="container">
+    <div class="textik">Otorinolaryngológ je odborníkom na:</div><hr>
+    <img src="obrázok19.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved19" value="Odpoved 1"  >
+            <label for="Odpoved1">Ochorenia ústnej sliznice</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved19" value="Odpoved 2">
+            <label for="Odpoved2">Ochorenia pľúc</label><br>
+
+            <input class="styl" type="radio" id="Odpoved3"  name="odpoved19" value="Odpoved 3">
+            <label for="Odpoved3">Chronické ochorenia všetkého druhu</label><br>
+
+            <input class="styl" type="radio" id="correct19"  name="odpoved19" value="Odpoved 4">
+            <label for="Odpoved4">Ochorenia uší, nosa, hrtana a hltana</label><br>
+        </div>
+    </div>
+    <button class="glow-on-hover" class="next" onclick="next(19)">NEXT</button>
+
+</div>
+
+
+<!--<otazka20-->
+
+<div class="container">
+    <div class="textik">Ktorý faktor sa najviac podieľa na vzniku ochorení pľúc?</div><hr>
+    <img src="obrázok20.jpg" width="450" height="400">
+
+    <div class="kon2">
+        <div class="radio-toolbar">
+            <input class="styl" type="radio" id="Odpoved1"  name="odpoved20" value="Odpoved 1"  >
+            <label for="Odpoved1">Strava s vysokým obsahom tuku</label><br>
+
+            <input class="styl" type="radio" id="Odpoved2"  name="odpoved20" value="Odpoved 2">
+            <label for="Odpoved2">Strava s vysokým obsahom sodíka</label><br>
+
+            <input class="styl" type="radio" id="correct20"  name="odpoved20" value="Odpoved 3">
+            <label for="Odpoved3"> Fajčenia</label><br>
+
+            <input class="styl" type="radio" id="Odpoved4 "  name="odpoved20" value="Odpoved 4">
+            <label for="Odpoved4">Konzumácia alkoholu</label><br>
+        </div>
+    </div>
     <button class="glow-on-hover"  onclick="result()">Vyhodnotiť</button>
 
 </div>
 
 
-
+<!--koniec-->
 
 <!--<div class="container">
     <div class="textik">Čo je na obrázku tri ?</div><hr>
@@ -298,6 +725,57 @@
             score++;
         }
         if (document.getElementById('correct3').checked) {
+            score++;
+        }
+        if (document.getElementById('correct4').checked) {
+            score++;
+        }
+        if (document.getElementById('correct5').checked) {
+            score++;
+        }
+        if (document.getElementById('correct6').checked) {
+            score++;
+        }
+        if (document.getElementById('correct7').checked) {
+            score++;
+        }
+        if (document.getElementById('correct8').checked) {
+            score++;
+        }
+        if (document.getElementById('correct9').checked) {
+            score++;
+        }
+        if (document.getElementById('correct10').checked) {
+            score++;
+        }
+        if (document.getElementById('correct11').checked) {
+            score++;
+        }
+        if (document.getElementById('correct12').checked) {
+            score++;
+        }
+        if (document.getElementById('correct13').checked) {
+            score++;
+        }
+        if (document.getElementById('correct14').checked) {
+            score++;
+        }
+        if (document.getElementById('correct15').checked) {
+            score++;
+        }
+        if (document.getElementById('correct16').checked) {
+            score++;
+        }
+        if (document.getElementById('correct17').checked) {
+            score++;
+        }
+        if (document.getElementById('correct18').checked) {
+            score++;
+        }
+        if (document.getElementById('correct19').checked) {
+            score++;
+        }
+        if (document.getElementById('correct20').checked) {
             score++;
         }
 
